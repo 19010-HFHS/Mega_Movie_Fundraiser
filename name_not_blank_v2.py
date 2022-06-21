c9f5c1a4dc9c804b9034b2cbe1f5b5abb0fa51b3
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-06-21 23:26:17 +0000

    added version 2 of "name not blank" function.

---
+++ name_not_blank_v2.py
@@ -0,0 +1,16 @@
+#Functions
+
+def not_blank(question, error_message)
+  valid = False
+
+  while not valid
+    response = input(question)
+
+    if response != "":
+      return response
+    else:
+      print(error_message)
+
+#Main Routine
+
+name = not_blank("Name: ", "Sorry, this cannot be blank - please enter your name")
