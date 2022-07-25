109a1e3901a85d38740ffac3cdce73a56f680da8
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-07-25 02:50:59 +0000

    Changed code layout and tested ticket_loop function

--- MMF.py
+++ MMF.py
@@ -1,43 +1,20 @@
-#  import statements
+#  start of loop
 
+#  initialise loop so that it runs at least once
+name = ""
+count = 0
+MAX_TICKETS = 5
 
-#  functions
+while name != "xxx" and count < MAX_TICKETS:
+  print("You have {} seats "
+        "left".format(MAX_TICKETS - count))
 
-#  'name not blank' function
-#  check name not blank
-def not_blank(question):
-  valid = False
+  #  Get details...
+  name = input("Name: ")
+  count += 1
+  print()
 
-  #  print error message if name is blank
-  while not valid:
-    response = input(question)
-
-    if response != "":
-      return response
-    else:
-      print("Sorry - this cannot be blank, please enter your name")
-
-#  --------Main Routine--------
-
-#  Set up dictionaries / lists needed to hold data
-
-#  Ask user if they have used the program before and show instructions if they haven't
-
-#  Loop to get ticket details
-
-  #  Get name (can't be blank)
-name = not_blank("Name: ")
-  #  Get age (between 12 and 130)
-  
-  #  Calculate ticket price
-  
-  #  Loop to ask for snacks
-
-  #  Calculate snack price
-
-  #  ask for payment method (and apply surcharge if necessary)
-
-
-#  Calculate Total sales and profit
-
-#  Output data to text file
+if count == MAX_TICKETS:
+  print("You have sold all the available tickets!")
+else: print("You have sold {} tickets. \n"
+           "there are {} places still available".format(count, MAX_TICKETS - count))
