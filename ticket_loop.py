6ae5725af354afea534d4cb6faa2cb5e7e42f57a
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-07-26 22:52:57 +0000

    fixed errors in ticket loop function

--- ticket_loop.py
+++ ticket_loop.py
@@ -7,14 +7,15 @@ MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
   print("You have {} seats "
-        "left".format(MAX_TICKETS - count))
+  "left".format(MAX_TICKETS - count))
 
-  #  Get details...
-  name = input("Name: ")
-  count += 1
-  print()
+
+#  Get details...
+name = input("Name: ")
+count += 1
+print()
 
 if count == MAX_TICKETS:
   print("You have sold all the available tickets!")
 else: print("You have sold {} tickets. \n"
-           "there are {} places still available".format(count, MAX_TICKETS - count))
+ "there are {} places still available".format(count, MAX_TICKETS - count))
