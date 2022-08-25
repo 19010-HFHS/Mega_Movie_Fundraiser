f5b595a3d1ea17b6e563ea092965c5c7a534a28b
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-08-25 00:32:50 +0000

    completed and tested ticket price component

--- MMF.py
+++ MMF.py
@@ -1,78 +1,26 @@
-#  'name not blank' function
-#  check name not blank
-def not_blank(question):
-  valid = False
+profit = 0
 
-  #  print error message if name is blank
-  while not valid:
-    response = input(question)
-
-    if response != "":
-      return response
-    else:
-      print("Sorry - this cannot be blank, please enter your name")
-
-#checks for an integer that is more than 0
-def int_check(question):
-
-  #  error message
-  error = "Please enter a whole number that is more than zero"
-  
-  valid = False
-  while not valid:
-   #  ask user for number and check it is valid
-    try:
-      response = int(input(question))
-      
-      if response <= 0:
-        print(error)
-      else:
-        return response
-        
-    #  if an integer is not entered, display an error
-    except ValueError:
-      print(error)
-# ********** Main Routine ************
-
-# Set up dictionaries / lists needed to hold data
-
-# Ask user if they have used the program before & show instructions if necessary
-
-# Loop to get ticket details
-# start of loop
-
-# initialise loop so that it runs at least once
 name = ""
-count = 0
-MAX_TICKETS = 5
+while name != "xxx":
 
-while name != "xxx" and count < MAX_TICKETS:
-#  Set up dictionaries / lists needed to hold data
-    if count < 4: 
-      print("You have {} seats "
-      "left".format(MAX_TICKETS - count))
-  # warns the user only one seat is left
-    else:
-      print("*** THERE IS ONE SEAT LEFT!! ***")
-      
-  #  Get name (can't be blank)
-    name = not_blank("Name: ")
-  #  end the loop if the exit code is entered
-    if name == "xxx":
-      break
-#  Get age (between 12 and 130)
-    age = int_check("Age: ")
-#  ckeck that age is valid...
-    if age < 12:
-      print("Sorry, you are too young for this movie")
-      continue
-    elif age > 130:
-      print("That is very old, it seems there's been a mistake")
-      continue
+  name = input("Name: ") # replace with function call
 
-    count += 1
+  # If name is exit code, break out of loop
+  if name == "xxx":
+    break
 
-if count == MAX_TICKETS:
-  print("You have sold all the available tickets!")
-else: print("You have sold {} tickets. \n"
- "there are {} seats still available".format(count, MAX_TICKETS - count))
+  age = int(input("Age: ")) # replace with function call
+
+  if age < 16:
+    ticket_price = 7.5
+  elif age >= 65:
+    ticket_price = 6.5
+  else:
+    ticket_price = 10.5
+
+  profit_made = ticket_price - 5
+  profit += profit_made
+
+  print("{} : ${:.2f}".format(name, ticket_price))
+
+print("Profit from Tickets: ${:.2f}".format(profit))
