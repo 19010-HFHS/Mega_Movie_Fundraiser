53e6d1be8d3c4c08140034dd54b5f9ee5547a5cc
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-08-30 23:17:55 +0000

    Added third variant of Yes_No function

--- MMF.py
+++ MMF.py
@@ -1,24 +1,27 @@
-#  defines function
-def yes_no(question):
-  #  error message
-  error = "Please answer yes / no"
+#  String Checking function, takes in question and list of valid responses
+def string_checker(question, to_check):
   
   valid = False
   while not valid:
 
-    #ask question and put response in lowercase
     response = input(question).lower()
 
-    if response == "yes" or response == "y":
-      return "yes"
-    elif response == "no" or response == "n":
-      return "no"
+    if response in to_check:
+      return response
+
     else:
-      print(error)
+      for item in to_check:
+      #  checks if response is the first letter of an item in the list
+        if response == item[0]:
+        #  note: returns the entire response rather than just the first letter
+          return item
+
+    print("sorry that is not a valid response")
+
 
 #  Main routine goes here
 
 for item in range(0, 6):
-  want_snacks = yes_no("Do you want snacks? ")
+  want_snacks = string_checker("Do you want snacks? ", ["yes", "no"])
   print("Answer OK, you said:", want_snacks)
   print()
